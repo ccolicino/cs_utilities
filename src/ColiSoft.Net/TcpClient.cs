365316887f20c85866ce9992315edac5ac667c91
Chris Colicino <ccolicino@gmail.com>
2016-10-23 13:21:41 -0400

    Added interfaces

--- src/ColiSoft.Net/TcpClient.cs
+++ src/ColiSoft.Net/TcpClient.cs
@@ -1,6 +1,8 @@
+using System.Threading.Tasks;
+
 namespace ColiSoft.Net
 {
-    public class TcpClient 
+    public class TcpClient : ITcpClient
     {
         private System.Net.Sockets.TcpClient _TcpClient;
         public TcpClient(System.Net.Sockets.TcpClient tcpClient)
